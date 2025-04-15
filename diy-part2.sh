9caf5066129f8b0e27987f501db4e25439ed91ce
JerryKy <lhsxc4@126.com>
2025-04-15 21:05:43 +0800

    更新argon

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,3 +2,7 @@
 
 # Modify hostname
 sed -i 's/ImmortalWrt/JerryWrt/g' package/base-files/files/bin/config_generate
+
+# 更新argon
+rm -rf package/feeds/luci/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/feeds/luci/luci-theme-argon
