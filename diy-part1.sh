a56f74a1d8495327f3bbe5b4b093d22b65b3994c
JerryKy <lhsxc4@126.com>
2025-04-15 17:53:10 +0800

    Create diy-part1.sh

---
+++ diy-part1.sh
@@ -0,0 +1,5 @@
+# 添加JerryKy-packages
+echo 'src-git jerry https://github.com/JerryKy/Jerrywrt-packages.git' >>feeds.conf.default
+
+# 添加lucky
+echo 'src-git lucky https://github.com/gdy666/luci-app-lucky.git' >>feeds.conf.default
