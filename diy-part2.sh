026a7a5fd25e20b4c9bfdee1f430cd0be2a11bac
JerryKy <lhsxc4@126.com>
2025-04-16 18:42:29 +0800

    修改默认ip

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,8 +13,12 @@ cat >>package/emortal/default-settings/files/99-default-settings-chinese<< EOF
 uci set luci.main.mediaurlbase='/luci-static/argon'
 uci commit luci
 
+uci set network.lan.ipaddr='192.168.50.132'
+uci commit network
+
 exit 0
 EOF
 
 # 自定义登录背景图片
 [ -e $GITHUB_WORKSPACE/images/bg1.jpg ] && mv $GITHUB_WORKSPACE/images/bg1.jpg package/feeds/luci/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+[ -e $GITHUB_WORKSPACE/images/KY_icon.svg ] && mv $GITHUB_WORKSPACE/images/KY_icon.svg package/feeds/luci/luci-theme-argon/htdocs/luci-static/argon/img/argon.svg
